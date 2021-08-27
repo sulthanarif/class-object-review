aec1ceda92c6c0860830148b9ae6a32c3f260b05
sulthanarif <msarizky201@gmail.com>
2021-08-27 10:33:03 +0700

    tugas objek ripiu

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // Tulis kode di bawah ini
+
+// membuat objek
+const kucing = {
+     rasKucing = "kampung",
+     umurKucing = 5,
+     kakiKucing = 4,
+     kondisi = true,
+     tubuhKucing = ['mata','badan','ekor','kepala','kumis'],
+     
+     cekKondisi = function(){
+          if(kondisi === true){
+               return 'kucing sehat';
+          } else {
+               return 'kucing sakit';
+          }
+     }
+     
+        
+
+
+
+}
+// memanggil objek
+// membuat class
+// memanggil classff
